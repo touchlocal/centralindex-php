47aa67128f0cd1ad9d5e11038f54ba59aea7ce7b
Glynn Bird <glynn@scoot.co.uk>
2013-06-17 09:33:13 +0100

    latest commit

--- CentralIndex.php
+++ CentralIndex.php
@@ -1300,6 +1300,25 @@
 
 
   /**
+   * Ring the person and verify their account
+   *
+   *  @param to - The phone number to verify
+   *  @param from - The phone number to call from
+   *  @param pin - The pin to verify the phone number with
+   *  @param language - The language to read the verification in
+   *  @return - the data from the api
+  */
+  public function getToolsPhonecallVerify( $to, $from, $pin, $language) {
+    $params = array();
+    $params['to'] = $to;
+    $params['from'] = $from;
+    $params['pin'] = $pin;
+    $params['language'] = $language;
+    return CentralIndex::doCurl("GET","/tools/phonecall/verify",$params);
+  }
+
+
+  /**
    * Given a spreadsheet id add a row
    *
    *  @param spreadsheet_key - The key of the spreadsheet to edit
@@ -1558,6 +1577,21 @@
     $params['locations_to_add'] = $locations_to_add;
     $params['locations_to_remove'] = $locations_to_remove;
     return CentralIndex::doCurl("POST","/entity/advertiser/location",$params);
+  }
+
+
+  /**
+   * Get all advertisers that have been updated from a give date for a given reseller
+   *
+   *  @param from_date
+   *  @param country
+   *  @return - the data from the api
+  */
+  public function getAdvertiserUpdated( $from_date, $country) {
+    $params = array();
+    $params['from_date'] = $from_date;
+    $params['country'] = $country;
+    return CentralIndex::doCurl("GET","/advertiser/updated",$params);
   }
 
 
